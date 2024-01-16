91f565004b7a196e97edbb30b2f6c858b997b3e0
Craigshakesphere-Makinde <117420707+Craigshakesphere-Makinde@users.noreply.github.com>
2024-01-16 12:50:17 +0100

    Create ActionBaseState.cs

---
+++ First Person Shooter/Assets/Script/Actions/ActionBaseState.cs
@@ -0,0 +1,7 @@
+
+public abstract class ActionBaseState
+{
+    public abstract void EnterState(ActionsStateManager manager);
+
+    public abstract void UpdateState(ActionsStateManager manager);
+}
